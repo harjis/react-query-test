f5a34409e6f93901fd9b6b88b683a61a2472b2d6
Joonas Harjukallio <harjukallio@gmail.com>
2021-11-26 09:22:30 +0200

    Test without msw2

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,24 +1,25 @@
-import { rest } from "msw";
-import { Post } from "../types";
-
-let blogPost: Post = {
-  id: 1,
-  name: "First post",
-  publishedAt: "2000-01-01T10:00:00Z",
-};
-
-export const getPost = rest.get("/post", (req, res, context) => {
-  return res(context.delay(1000), context.json(blogPost));
-});
-
-export const updatePost = rest.patch<Partial<Omit<Post, "id">>>(
-  "/post",
-  (req, res, context) => {
-    const changes = req.body;
-    blogPost = { ...blogPost, ...changes };
-
-    return res(context.delay(1000), context.json(blogPost));
-  }
-);
-
-export const handlers = [getPost, updatePost];
+// import { rest } from "msw";
+// import { Post } from "../types";
+//
+// let blogPost: Post = {
+//   id: 1,
+//   name: "First post",
+//   publishedAt: "2000-01-01T10:00:00Z",
+// };
+//
+// export const getPost = rest.get("/post", (req, res, context) => {
+//   return res(context.delay(1000), context.json(blogPost));
+// });
+//
+// export const updatePost = rest.patch<Partial<Omit<Post, "id">>>(
+//   "/post",
+//   (req, res, context) => {
+//     const changes = req.body;
+//     blogPost = { ...blogPost, ...changes };
+//
+//     return res(context.delay(1000), context.json(blogPost));
+//   }
+// );
+//
+// export const handlers = [getPost, updatePost];
+export {};
